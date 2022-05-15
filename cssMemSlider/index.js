5d05eaa88e014a41332b9d9fe8bada3213e65d08
denis.borushko <denis.borushko@vizor-games.com>
2022-05-15 17:53:52 +0300

    feat: git ignore added (branch: gh-pages, date: 05/15/22 17:53:53)

---
+++ cssMemSlider/index.js
@@ -0,0 +1,24 @@
+const mems = [
+  {
+    img: "./assets/images/sueta.jpg",
+    text: "Хочется навести суету"
+  },
+  {
+    img: "./assets/images/sueta.jpg",
+    text: "Хочется навести суету"
+  },
+  {
+    img: "./assets/images/sueta.jpg",
+    text: "Хочется навести суету"
+  },
+  {
+    img: "./assets/images/sueta.jpg",
+    text: "Хочется навести суету"
+  }
+]
+
+function handleWindowLoad() {
+
+}
+
+window.addEventListener('load', handleWindowLoad);
